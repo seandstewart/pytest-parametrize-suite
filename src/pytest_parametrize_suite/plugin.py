e8b70567e00bd00b2c002a14ff6e235de7209a7a
Sean Stewart <sean_stewart@me.com>
2023-01-19 07:47:46 -0500

    Add support for multiple `suite` markers to generate a test matrix.

--- src/pytest_parametrize_suite/plugin.py
+++ src/pytest_parametrize_suite/plugin.py
@@ -9,23 +9,21 @@ __all__ = ("pytest_generate_tests", "pytest_configure")
 
 def pytest_generate_tests(metafunc: pytest.Metafunc):
     func: pytest.Function = metafunc.definition
-    matrix_marker = next((m for m in func.own_markers if m.name == MARKER_NAME), None)
+    suites = [m for m in func.own_markers if m.name == MARKER_NAME and m.kwargs]
     # If there is no marker, there's nothing to do.
-    if matrix_marker is None:
+    if not suites:
         return
 
-    # If the test is marked, but no matrix was provided, there's nothing to do.
-    test_suite = matrix_marker.kwargs
-    if not test_suite:
-        return
-
-    # Gather the test suite into a format pytest understands.
-    ids, argnames, argvalues = gather_test_cases(test_suite=test_suite)
-    metafunc.parametrize(
-        argnames=argnames,
-        argvalues=argvalues,
-        ids=ids,
-    )
+    for marker in suites:
+        # If the test is marked, but no matrix was provided, there's nothing to do.
+        test_suite = marker.kwargs
+        # Gather the test suite into a format pytest understands.
+        ids, argnames, argvalues = gather_test_cases(test_suite=test_suite)
+        metafunc.parametrize(
+            argnames=argnames,
+            argvalues=argvalues,
+            ids=ids,
+        )
 
 
 def pytest_configure(config: pytest.Config):
--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -14,6 +14,31 @@ def test_valid_suite(arg1, arg2):
     assert result == expected_result
 
 
+@pytest.mark.suite(
+    case1=dict(arg1="cross1"),
+    case2=dict(arg1="cross2"),
+)
+@pytest.mark.suite(
+    case3=dict(arg2="product1"),
+    case4=dict(arg2="product2"),
+)
+def test_suite_matrix(arg1, arg2):
+    # Given
+    combination = arg1 + arg2
+    # When
+    possible_combinations.remove(combination)
+    # Then
+    assert combination not in possible_combinations
+
+
+possible_combinations = {
+    "cross1product1",
+    "cross1product2",
+    "cross2product1",
+    "cross2product2",
+}
+
+
 def test_no_marker():
     assert True
 
